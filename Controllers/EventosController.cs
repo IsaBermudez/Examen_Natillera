f2d3a1e8f2daf9917ddf897d0bd2d92b91919248
IsaBermudez <isabarboleda@yahoo.com>
2025-04-22 22:07:58 -0500

    Modificacion controller

--- Controllers/EventosController.cs
+++ Controllers/EventosController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Examen_Natillera.Clases;
+using Examen_Natillera.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,73 +9,73 @@ using System.Web.Http;
 
 namespace Examen_Natillera.Controllers
 {
+
+    [RoutePrefix("api/Eventos")]
     public class EventosController : ApiController
     {
-        [RoutePrefix("api/Eventos")]
-        public class EventosController : ApiController
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<Evento> ConsultarTodos()
         {
-            [HttpGet]
-            [Route("ConsultarTodos")]
-            public List<Evento> ConsultarTodos()
-            {
-                clsEventos Evento = new clsEventos();
-                return Evento.ConsultarTodos();
-            }
+            clsEventos Evento = new clsEventos();
+            return Evento.ConsultarTodos();
+        }
 
-            [HttpGet]
-            [Route("Consultar")]
-            public Evento consultar(int id)
-            {
-                clsEventos Evento = new clsEventos();
-                return Evento.Consultar(id);
-            }
+        [HttpGet]
+        [Route("Consultar")]
+        public Evento consultar(int id)
+        {
+            clsEventos Evento = new clsEventos();
+            return Evento.Consultar(id);
+        }
 
-            [Route("ConsultarXTipo")]
-            public List<Evento> ConsultarXTipo(string tipo)
-            {
-                clsEventos Evento = new clsEventos();
-                return Evento.ConsultarXTipo(tipo);
-            }
+        [Route("ConsultarXTipo")]
+        public List<Evento> ConsultarXTipo(string tipo)
+        {
+            clsEventos Evento = new clsEventos();
+            return Evento.ConsultarXTipo(tipo);
+        }
 
 
-            [Route("ConsultarXNombre")]
-            public Evento ConsultarXNombre(string nombre)
-            {
-                clsEventos Evento = new clsEventos();
-                return Evento.ConsultarXNombre(nombre);
-            }
+        [Route("ConsultarXNombre")]
+        public Evento ConsultarXNombre(string nombre)
+        {
+            clsEventos Evento = new clsEventos();
+            return Evento.ConsultarXNombre(nombre);
+        }
 
-            [Route("ConsultarPorFecha")]
-            public List<Evento> ConsultarEventosPorFecha(DateTime fecha)
-            {
-                clsEventos Evento = new clsEventos();
-                return Evento.ConsultarEventosPorFecha(fecha );
-            }
+        [Route("ConsultarPorFecha")]
+        public List<Evento> ConsultarEventosPorFecha(DateTime fecha)
+        {
+            clsEventos Evento = new clsEventos();
+            return Evento.ConsultarEventosPorFecha(fecha);
+        }
 
-            [HttpPost]
-            [Route("Insertar")]
-            public String Insertar([FromBody] Evento Even)
-            {
-                clsEventos Evento = new clsEventos();
-                Evento.evento = Even;
-                return Evento.Insertar();
-            }
+        [HttpPost]
+        [Route("Insertar")]
+        public String Insertar([FromBody] Evento Even)
+        {
+            clsEventos Evento = new clsEventos();
+            Evento.evento = Even;
+            return Evento.Insertar();
+        }
 
-            [HttpPut]
-            [Route("Actualizar")]
-            public String Actualizar([FromBody] Evento Even)
-            {
-                clsEventos Evento = new clsEventos();
-                Evento.evento = Even;
-                return Evento.Actualizar();
-            }
+        [HttpPut]
+        [Route("Actualizar")]
+        public String Actualizar([FromBody] Evento Even)
+        {
+            clsEventos Evento = new clsEventos();
+            Evento.evento = Even;
+            return Evento.Actualizar();
+        }
 
-            [HttpDelete]
-            [Route("Eliminar")]
-            public String Eliminar(int id)
-            {
-                clsEventos Evento = new clsEventos();
-                return Evento.Eliminar(id);
-            }
+        [HttpDelete]
+        [Route("Eliminar")]
+        public String Eliminar(int id)
+        {
+            clsEventos Evento = new clsEventos();
+            return Evento.Eliminar(id);
         }
     }
+}
+    
