762aa4b93f109b1ca8356d37b5e0ca41de5bbea4
GereSanta07 <geresanta@hotmail.com>
2025-04-23 00:47:10 -0500

    Creacion de la clase login, Login controler y clsLogin

---
+++ Clases/clsLogin.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen_Natillera.Clases
+{
+    public class clsLogin
+    {
+        public clsLogin()
+        {
+            loginRespuesta = new LoginRespuesta();
+        }
+        public Login login { get; set; }
+        public LoginRespuesta loginRespuesta { get; set; }
+
+
+        public bool ValidarUsuario()
+        {
+            try
+            {
+                Administrador admi = dbExamen.Administradors.FirstOrDefault(u => u.Usuario == login.Usuario);
+                if (admi == null)
+                {
+                    // el usuario no existe, se retorna un error 
+                    loginRespuesta.Autenticado = false;
+                    loginRespuesta.Mensaje = "El administrador no existe";
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                loginRespuesta.Autenticado = false;
+                loginRespuesta.Mensaje = ex.Message;
+                return false;
+            }
+        }
+        private bool ValidarClave()
+        {
+            try
+            {
+                Administrador admi = dbExamen.Administradors.FirstOrDefault(u => u.Usuario == login.Usuario && u.Clave == login.Clave);
+                if (admi == null)
+                {
+                    // la clave no es igual 
+                    loginRespuesta.Autenticado = false;
+                    loginRespuesta.Mensaje = "La clave no coincide";
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                loginRespuesta.Autenticado = false;
+                loginRespuesta.Mensaje = ex.Message;
+                return false;
+            }
+        }
+
+        public IQueryable<LoginRespuesta> Ingresar()
+        {
+            if (ValidarUsuario() && ValidarClave())
+            {
+                string token = TokenGenerator.GenerateTokenJwt(login.Usuario);
+                return from U in dbExamen.Set<Administrador>()
+                       where U.Usuario == login.Usuario &&
+                               U.Clave == login.Clave
+                       select new LoginRespuesta
+                       {
+                           Usuario = U.Usuario,
+                           Autenticado = true,
+                           Token = token,
+                           Mensaje = ""
+                       };
+            }
+            else
+            {
+                List<LoginRespuesta> List = new List<LoginRespuesta>();
+                List.Add(loginRespuesta);
+                return List.AsQueryable();
+            }
+        }
+    }
+}
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Examen_Natillera.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [RoutePrefix("api/Login")]
+        [AllowAnonymous]
+        public class LoginController : ApiController
+        {
+            [HttpPost]
+            [Route("Ingresar")]
+
+            public IQueryable<LoginRespuesta> Ingresar(Login login)
+            {
+                clsLogin _Login = new clsLogin();
+                _Login.login = login;
+                return _Login.Ingresar();
+            }
+        }
+    }
+}
